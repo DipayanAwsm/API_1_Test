fb58a5d6cf1df9174253fa8a04c7dc199e79323a
Dipayan Das <picku.pickudas@gmail.com>
2025-08-05 22:35:15 +0530

    Fix for Render port binding

--- main.py
+++ main.py
@@ -11,9 +11,3 @@ def read_root():
 def hello(name: str):
     return {"greeting": f"Hello, {name}!"}
 
-if __name__ == "__main__":
-    print("In Main Deploy")
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
-    
-    
